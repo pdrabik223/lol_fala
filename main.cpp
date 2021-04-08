61dfa8fb78c1dd6e3d69999b28820200e32e8e1c
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-04-08 23:24:25 +0200

    window is now a function, and std::thread does not work for some weird reason

--- main.cpp
+++ main.cpp
@@ -2,18 +2,13 @@
 // Created by studio25 on 07.04.2021.
 //
 #include <iostream>
+#include <thread>
 #include "SDL2/SDL.h"
 
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 600
 
-
-int main(int argc, char *argv[]){
-
-    std::cout<<"lol wtf";
-// 44,1 kHz
-// 44100 times per second
-// 60 fps <- that's me
+void window_with_line(int* ptr_to_data ){
     SDL_Event event;
     SDL_Renderer *renderer;
     SDL_Window *window;
@@ -40,7 +35,21 @@ int main(int argc, char *argv[]){
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    return EXIT_SUCCESS;
+
+}
 
 
+int main(int argc, char *argv[]){
+
+    std::cout<<"lol wtf";
+// 44,1 kHz
+// 44100 times per second
+// 60 fps <- that's me
+    window_with_line(nullptr);
+
+   std::thread main_window (window_with_line, nullptr);
+
+
+    maifreadow.join();
+return 0;
 }
